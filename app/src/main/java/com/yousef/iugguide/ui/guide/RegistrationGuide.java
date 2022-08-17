6a310d925a477882a813016c32c34e9d2e633fe1
Yousef <yousefokhrais@gmail.com>
2022-08-17 13:46:47 +0300

    Added RegistrationGuide :Steps 1-2-3-4

--- app/src/main/java/com/yousef/iugguide/ui/guide/RegistrationGuide.java
+++ app/src/main/java/com/yousef/iugguide/ui/guide/RegistrationGuide.java
@@ -4,16 +4,81 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.yousef.iugguide.R;
 import com.yousef.iugguide.databinding.ActivityRegistrationGuideBinding;
+import com.yousef.iugguide.ui.guide.steps.CompleteRegistrationFormStep;
+import com.yousef.iugguide.ui.guide.steps.GoToAdmissionAndRegistrationStep;
+import com.yousef.iugguide.ui.guide.steps.GoToNewStudentsPlaceStep;
+import com.yousef.iugguide.ui.guide.steps.GoToUniversityStep;
+import com.yousef.iugguide.ui.guide.steps.PayApplicationFeeStep;
+import com.yousef.iugguide.ui.guide.steps.PayTuitionFeesStep;
+import com.yousef.iugguide.ui.guide.steps.RequiredPaperworkStep;
 
-public class RegistrationGuide extends AppCompatActivity {
+import ernestoyaquello.com.verticalstepperform.Step;
+import ernestoyaquello.com.verticalstepperform.VerticalStepperFormView;
+import ernestoyaquello.com.verticalstepperform.listener.StepperFormListener;
+
+public class RegistrationGuide extends AppCompatActivity implements StepperFormListener {
 
     ActivityRegistrationGuideBinding binding;
+    private VerticalStepperFormView stepper_form;
+
+
+    private GoToUniversityStep goToUniversityStep;
+    private GoToNewStudentsPlaceStep goToNewStudentsPlaceStep;
+    private RequiredPaperworkStep requiredPaperworkStep;
+    private PayApplicationFeeStep payApplicationFeeStep;
+    private CompleteRegistrationFormStep completeRegistrationFormStep;
+    private PayTuitionFeesStep payTuitionFeesStep;
+    private GoToAdmissionAndRegistrationStep goToAdmissionAndRegistrationStep;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityRegistrationGuideBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        stepper_form = findViewById(R.id.stepper_form);
+
+        goToUniversityStep = new GoToUniversityStep("التوجه إلى الجامعة الإسلامية");
+        goToNewStudentsPlaceStep = new GoToNewStudentsPlaceStep("الذهاب الى مكان استقبال الطلبة الجدد");
+        requiredPaperworkStep = new RequiredPaperworkStep("الوثائق المطلوبة");
+        payApplicationFeeStep = new PayApplicationFeeStep("دفع رسوم طلب الالتحاق");
+        completeRegistrationFormStep = new CompleteRegistrationFormStep("تعبئة طلب الالتحاق");
+        payTuitionFeesStep = new PayTuitionFeesStep("دفع الرسوم الدراسية");
+        goToAdmissionAndRegistrationStep = new GoToAdmissionAndRegistrationStep("الحصول على الجدول الدراسي");
+
+        stepper_form
+                .setup(this,
+                        goToUniversityStep,
+                        goToNewStudentsPlaceStep,
+                        requiredPaperworkStep,
+                        payApplicationFeeStep,
+                        completeRegistrationFormStep,
+                        payTuitionFeesStep,
+                        goToAdmissionAndRegistrationStep
+                )
+                .includeConfirmationStep(false)
+                .init();
+    }
+
+    @Override
+    public void onCompletedForm() {
+
+    }
+
+    @Override
+    public void onCancelledForm() {
+
+    }
+
+    @Override
+    public void onStepAdded(int index, Step<?> addedStep) {
+
+    }
+
+    @Override
+    public void onStepRemoved(int index) {
+
     }
 }
---
+++ app/src/main/java/com/yousef/iugguide/ui/guide/steps/GoToUniversityStep.java
@@ -0,0 +1,65 @@
+package com.yousef.iugguide.ui.guide.steps;
+
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.yousef.iugguide.R;
+
+import ernestoyaquello.com.verticalstepperform.Step;
+
+public class GoToUniversityStep extends Step<String> {
+
+    private View stepContent;
+
+    public GoToUniversityStep(String stepTitle) {
+        super(stepTitle);
+    }
+
+    @Override
+    protected View createStepContentLayout() {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        stepContent = inflater.inflate(R.layout.step_1_go_to_university_layout, null, false);
+
+        return stepContent;
+    }
+
+    @Override
+    protected IsDataValid isStepDataValid(String stepData) {
+        return new IsDataValid(true);
+    }
+
+    @Override
+    public String getStepData() {
+        return "";
+    }
+
+    @Override
+    public String getStepDataAsHumanReadableString() {
+        return "";
+    }
+
+    @Override
+    protected void onStepOpened(boolean animated) {
+        // This will be called automatically whenever the step gets opened.
+    }
+
+    @Override
+    protected void onStepClosed(boolean animated) {
+        // This will be called automatically whenever the step gets closed.
+    }
+
+    @Override
+    protected void onStepMarkedAsCompleted(boolean animated) {
+        // This will be called automatically whenever the step is marked as completed.
+    }
+
+    @Override
+    protected void onStepMarkedAsUncompleted(boolean animated) {
+        // This will be called automatically whenever the step is marked as uncompleted.
+    }
+
+    @Override
+    protected void restoreStepData(String stepData) {
+        // To restore the step after a configuration change, we restore the text of its EditText view.
+    }
+}
