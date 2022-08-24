4801df5a71dea3ca5b39643923eb85d85777e0b3
Yousef <yousefokhrais@gmail.com>
2022-08-24 13:31:43 +0300

    Disabled dark mode by default

--- app/src/main/java/com/yousef/iugguide/AppClass.java
+++ app/src/main/java/com/yousef/iugguide/AppClass.java
@@ -2,6 +2,8 @@ package com.yousef.iugguide;
 
 import android.app.Application;
 
+import androidx.appcompat.app.AppCompatDelegate;
+
 import com.yousef.iugguide.helpers.DataHelper;
 import com.yousef.iugguide.models.Building;
 import com.yousef.iugguide.models.College;
@@ -14,13 +16,15 @@ public class AppClass extends Application {
     public static ArrayList<Department> departmentsArrayList = new ArrayList<>();
     public static ArrayList<College> collegesArrayList = new ArrayList<>();
     public static ArrayList<Building> buildingArrayList = new ArrayList<>();
-    public static  College chosen_college;
-    public static  Department chosen_Department;
-    public static  Building chosenBuilding;
+    public static College chosen_college;
+    public static Department chosen_Department;
+    public static Building chosenBuilding;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        //This is added to disable dark mode by default
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         DataHelper.getInstance().readCollegesData(this);
         DataHelper.getInstance().readDepartmentsData(this);
