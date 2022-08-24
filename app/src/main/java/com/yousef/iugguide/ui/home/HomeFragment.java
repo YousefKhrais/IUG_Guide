af281946ef05134e60fed8294a4de77cc3d80a31
Yousef <yousefokhrais@gmail.com>
2022-08-24 15:56:58 +0300

    Changed some of the home items icons

--- app/src/main/java/com/yousef/iugguide/ui/home/HomeFragment.java
+++ app/src/main/java/com/yousef/iugguide/ui/home/HomeFragment.java
@@ -39,19 +39,34 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         homeItems.add(new HomeItem("button_complaints_box", "صندوق فضفضة", R.drawable.ic_bachelor_icon));
         homeItems.add(new HomeItem("button_student_services", "خدمات الطالب", R.drawable.ic_bachelor_icon));
         homeItems.add(new HomeItem("button_business_incubator", "حاضنة الاعمال", R.drawable.ic_bachelor_icon));
-        homeItems.add(new HomeItem("button_alumni_affairs", "شؤون الخريجين", R.drawable.ic_bachelor_icon));
         homeItems.add(new HomeItem("button_library", "المكتبة", R.drawable.ic_bachelor_icon));
         homeItems.add(new HomeItem("button_map", "تجول في الجامعة", R.drawable.ic_bachelor_icon));
-        //reeeeeeeee
+        homeItems.add(new HomeItem("button_events", "الفعاليات", R.drawable.ic_bachelor_icon));
         homeItems.add(new HomeItem("button_registration_guide", "دليل التسجيل", R.drawable.ic_bachelor_icon));
-        homeItems.add(new HomeItem("button_acceptance_key", "مفتاح القبول", R.drawable.ic_bachelor_icon));
-        homeItems.add(new HomeItem("button_moodle", "مودل", R.drawable.ic_bachelor_icon));
-        homeItems.add(new HomeItem("button_university_website", "موقع الجامعة", R.drawable.ic_bachelor_icon));
-        homeItems.add(new HomeItem("button_admission_application", "طلب الالتحاق", R.drawable.ic_bachelor_icon));
 
+        //Done
+        homeItems.add(new HomeItem("button_alumni_affairs", "شؤون الخريجين", R.drawable.ic_graduate3));
+//        homeItems.add(new HomeItem("button_alumni_affairs", "شؤون الخريجين", R.drawable.ic_graduate1));
+//        homeItems.add(new HomeItem("button_alumni_affairs", "شؤون الخريجين", R.drawable.ic_graduate2));
+
+        homeItems.add(new HomeItem("button_faq", "أسئلة شائعة", R.drawable.ic_faq3));
+//        homeItems.add(new HomeItem("button_faq", "أسئلة شائعة", R.drawable.ic_faq1));
+//        homeItems.add(new HomeItem("button_faq", "أسئلة شائعة", R.drawable.ic_faq2));
+//        homeItems.add(new HomeItem("button_faq", "أسئلة شائعة", R.drawable.ic_faq4));
+
+        homeItems.add(new HomeItem("button_acceptance_key", "مفتاح القبول", R.drawable.ic_key));
+//        homeItems.add(new HomeItem("button_acceptance_key", "مفتاح القبول", R.drawable.ic_percentage));
+
+        homeItems.add(new HomeItem("button_announcements", "الاعلانات", R.drawable.ic_announcement));
+        homeItems.add(new HomeItem("button_moodle", "مودل", R.drawable.ic_moodle));
+        homeItems.add(new HomeItem("button_scholarships", "المنح الدراسية", R.drawable.ic_scholarships));
+        homeItems.add(new HomeItem("button_admission_application", "طلب الالتحاق", R.drawable.ic_admission_application));
+        homeItems.add(new HomeItem("button_volunteer_work", "العمل التطوعي", R.drawable.ic_volunteer));
+        homeItems.add(new HomeItem("button_university_website", "موقع الجامعة", R.drawable.ic_website));
+        //Done
         homeAdapter = new HomeAdapter(getActivity(), homeItems, this);
 
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 2, GridLayoutManager.VERTICAL, false);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 3, GridLayoutManager.VERTICAL, false);
         dataList.setLayoutManager(gridLayoutManager);
         dataList.setAdapter(homeAdapter);
 
@@ -66,29 +81,31 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
-        System.out.println("onclick: " + view.getId());
-        System.out.println("tag: " + view.getTag());
         switch ((String) view.getTag()) {
             case "button_registration_guide":
                 Intent intent = new Intent(getActivity(), RegistrationGuide.class);
                 getActivity().startActivity(intent);
                 break;
+
             case "button_acceptance_key":
                 intent = new Intent(getActivity(), AcceptanceKeys.class);
                 getActivity().startActivity(intent);
                 break;
+
             case "button_moodle":
                 String url = "https://moodle.iugaza.edu.ps";
                 Intent i = new Intent(Intent.ACTION_VIEW);
                 i.setData(Uri.parse(url));
                 startActivity(i);
                 break;
+
             case "button_university_website":
                 url = "http://iugaza.edu.ps";
                 i = new Intent(Intent.ACTION_VIEW);
                 i.setData(Uri.parse(url));
                 startActivity(i);
                 break;
+
             case "button_admission_application":
                 url = "https://eportal.iugaza.edu.ps/ords/f?p=142:LOGIN:12002841936534:::::";
                 i = new Intent(Intent.ACTION_VIEW);
@@ -99,23 +116,61 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
             case "button_university_facilities":
                 System.out.println("button_university_facilities");
                 break;
+
             case "button_complaints_box":
                 System.out.println("button_complaints_box");
                 break;
+
             case "button_student_services":
                 System.out.println("button_student_services");
                 break;
+
             case "button_business_incubator":
                 System.out.println("button_business_incubator");
                 break;
+
             case "button_alumni_affairs":
                 System.out.println("button_alumni_affairs");
                 break;
+
             case "button_library":
                 System.out.println("button_library");
                 break;
+
             case "button_map":
                 System.out.println("button_map");
+                break;
+            //reeeee
+            case "button_volunteer_work":
+                System.out.println("button_volunteer_work");
+                break;
+
+            case "button_scholarships":
+                url = "https://www.iugaza.edu.ps/%d9%85%d9%86%d8%ad-%d8%a7%d9%84%d8%ac%d8%a7%d9%85%d8%b9%d8%a9";
+                i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+                System.out.println("button_scholarships");
+                break;
+
+            case "button_faq":
+                System.out.println("button_faq");
+                break;
+
+            case "button_events":
+                url = "https://www.iugaza.edu.ps/%d8%a7%d9%84%d9%81%d8%b9%d8%a7%d9%84%d9%8a%d8%a7%d8%aa";
+                i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+                System.out.println("button_events");
+                break;
+
+            case "button_announcements":
+                url = "https://www.iugaza.edu.ps/e3lan";
+                i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+                System.out.println("button_announcements");
                 break;
         }
     }
