dbb0634d85d35c0c53645008bf47bc26b4265790
Yousef Khrais <yousefkh@students.iugaza.edu.ps>
2022-09-05 21:13:44 +0300

    Added InternetGuidePage

--- app/src/main/java/com/yousef/iugguide/ui/buildings/BuildingsFragment.java
+++ app/src/main/java/com/yousef/iugguide/ui/buildings/BuildingsFragment.java
@@ -2,12 +2,8 @@ package com.yousef.iugguide.ui.buildings;
 
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.net.ConnectivityManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,12 +13,10 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.yousef.iugguide.AppClass;
-import com.yousef.iugguide.MainActivity;
-import com.yousef.iugguide.R;
 import com.yousef.iugguide.adapters.BuildingAdapter;
 import com.yousef.iugguide.databinding.FragmentBuildingsBinding;
 import com.yousef.iugguide.databinding.InterntDialogBinding;
-import com.yousef.iugguide.ui.home.HomeFragment;
+import com.yousef.iugguide.ui.home.InternetGuideBottomSheetDialog;
 
 public class BuildingsFragment extends Fragment {
 
@@ -36,22 +30,30 @@ public class BuildingsFragment extends Fragment {
         binding.collegeRv.setLayoutManager(new LinearLayoutManager(this.getContext()));
         buildingAdapter = new BuildingAdapter(this.getContext(), AppClass.buildingArrayList);
         binding.collegeRv.setAdapter(buildingAdapter);
-if (!isNetworkConnected()){
-    InterntDialogBinding binding = InterntDialogBinding.inflate(getLayoutInflater());
-    AlertDialog.Builder alBuilder = new AlertDialog.Builder(this.getContext());
-    alBuilder.setTitle("alert");
 
-    alBuilder.setView(binding.getRoot());
-    final AlertDialog ad = alBuilder.show();
-    binding.show.setOnClickListener(v->{
-        startActivity(new Intent(this.getContext(), MainActivity.class));
-    });
-    binding.close.setOnClickListener(v->{
-        ad.dismiss();
-    });
+        if (!isNetworkConnected()) {
+            InterntDialogBinding binding = InterntDialogBinding.inflate(getLayoutInflater());
+            AlertDialog.Builder alBuilder = new AlertDialog.Builder(this.getContext());
+            alBuilder.setTitle("alert");
 
-}
+            alBuilder.setView(binding.getRoot());
+            final AlertDialog ad = alBuilder.show();
+            binding.show.setOnClickListener(v -> {
+                openInternetGuidePage();
+            });
+            binding.close.setOnClickListener(v -> {
+                ad.dismiss();
+            });
+
+        }
+
         return root;
+    }
+
+    private void openInternetGuidePage() {
+        InternetGuideBottomSheetDialog internetGuideBottomSheetDialog = new InternetGuideBottomSheetDialog();
+        internetGuideBottomSheetDialog.setCancelable(true);
+        internetGuideBottomSheetDialog.show(getChildFragmentManager(), "internetGuideBottomSheetDialog");
     }
 
     @Override
@@ -59,6 +61,7 @@ if (!isNetworkConnected()){
         super.onDestroyView();
         binding = null;
     }
+
     private boolean isNetworkConnected() {
         ConnectivityManager cm = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
         return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
